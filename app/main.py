6d5f3cb0108f980acc611cca550be8afdfaf68c3
thien <tribichthuthien@gmail.com>
2026-09-05 00:43:39 -0400

    Added pdf upload and text extraction

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from pypdf import PdfReader
+from io import BytesIO
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "AI Document Assistant is running"}
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    if file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Only PDF files are supported")
+
+    contents = await file.read()
+    pdf = PdfReader(BytesIO(contents))
+
+    text = ""
+
+    for page in pdf.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text + "\n"
+
+    return {
+        "filename": file.filename,
+        "text": text
+    }
