3dbb07718d09d89242ea5413bc718fcec915b682
Thien Luu <tribichthuthien@gmail.com>
2026-09-17 00:00:40 -0500

    re added chunks

--- app/document.py
+++ app/document.py
@@ -4,6 +4,8 @@ def chunk_text(text, chunk_size=1000, overlap=200):
 
     while start < len(text):
         end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
 
         start += chunk_size - overlap
 
